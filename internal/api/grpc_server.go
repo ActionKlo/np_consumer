2c34d36260ac7c636dda838cbdcd3300fb87b6d8
Andrei <actionclaw@gmail.com>
2024-02-17 22:18:02 +0100

    add logging

--- internal/api/grpc_server.go
+++ internal/api/grpc_server.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"context"
-	"fmt"
 	"github.com/google/uuid"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -20,9 +19,10 @@ type grpcServer struct {
 	logger *zap.Logger
 }
 
-func NewGRPCServer(DB *db.Service) *grpc.Server {
+func NewGRPCServer(DB *db.Service, logger *zap.Logger) *grpc.Server {
 	srv := grpcServer{
-		DB: DB,
+		DB:     DB,
+		logger: logger,
 	}
 
 	gsrv := grpc.NewServer()
@@ -34,11 +34,11 @@ func NewGRPCServer(DB *db.Service) *grpc.Server {
 }
 
 func (s *grpcServer) CreateReceiver(ctx context.Context, req *api.CreateReceiverRequest) (*api.CreateReceiverResponse, error) {
-	fmt.Println("create receiver")
-	if req.Receiver == nil {
-		err := status.Errorf(codes.NotFound, "request body is empty")
+	s.logger.Debug("call CreateReceiver method with data", zap.Any("data", req))
 
-		return nil, err
+	if req.Receiver == nil {
+		s.logger.Error("receiver has no data")
+		return nil, status.Errorf(codes.InvalidArgument, "request body is empty")
 	}
 
 	receiver := &gapi.Receiver{
@@ -48,57 +48,67 @@ func (s *grpcServer) CreateReceiver(ctx context.Context, req *api.CreateReceiver
 
 	rid, err := s.DB.CreateReceiver(ctx, receiver)
 	if err != nil {
-		return nil, err
+		s.logger.Error("failed to create receiver", zap.Error(err))
+		return nil, status.Errorf(codes.Internal, "filed to create receiver, got error: %v", err.Error())
 	}
 
+	s.logger.Debug("receiver created successful", zap.Any("receiver_id", rid))
 	return &api.CreateReceiverResponse{Rid: rid.String()}, nil
 }
 
 func (s *grpcServer) RetrieveReceiver(ctx context.Context, req *api.RetrieveReceiverRequest) (*api.RetrieveReceiverResponse, error) {
-	fmt.Println("retrieve receiver")
+	s.logger.Debug("call RetrieveReceiver method with data", zap.Any("data", req))
 
-	// TODO check empty data
-
-	var receiver *gapi.Receiver
-	receiver, err := s.DB.RetrieveReceiver(ctx, uuid.MustParse(req.Rid))
+	rid, err := uuid.Parse(req.Rid)
 	if err != nil {
-		return nil, err
+		s.logger.Error("failed to parse Rid to uuid format", zap.Error(err))
+		return nil, status.Errorf(codes.InvalidArgument, "failed to parse Rid \"%v\" to uuid format, got error: %v", req.Rid, err.Error())
 	}
 
+	var receiver *gapi.Receiver
+	receiver, err = s.DB.RetrieveReceiver(ctx, rid)
+	if err != nil {
+		s.logger.Error("failed to retrieve receiver", zap.Error(err))
+		return nil, status.Errorf(codes.Internal, "filed to retrieve receiver, got error: %v", err.Error())
+	}
+
+	s.logger.Debug("receiver retrieve successful", zap.Any("receiver", receiver))
 	return &api.RetrieveReceiverResponse{Receiver: receiver}, nil
 }
 
 func (s *grpcServer) UpdateReceiver(ctx context.Context, req *api.UpdateReceiverRequest) (*api.UpdateReceiverResponse, error) {
-	fmt.Println("update receiver")
+	s.logger.Debug("call UpdateReceiver method with data", zap.Any("data", req))
 
 	if req.Receiver == nil || req.Receiver.Url == "" || req.Receiver.Id == "" {
-		return nil, status.Errorf(codes.InvalidArgument, "receiver data to update is wrong")
+		s.logger.Error("invalid data to update receiver")
+		return nil, status.Errorf(codes.InvalidArgument, "invalid receiver data")
 	}
 
 	err := s.DB.UpdateReceiver(ctx, req.Receiver)
 	if err != nil {
-		return nil, err
+		s.logger.Error("failed to update receiver", zap.Error(err))
+		return nil, status.Errorf(codes.Internal, "failed to update receiver, got error: %v", err.Error())
 	}
 
+	s.logger.Debug("receiver updated successful")
 	return &api.UpdateReceiverResponse{}, nil
 }
 
 func (s *grpcServer) DeleteReceiver(ctx context.Context, req *api.DeleteReceiverRequest) (*api.DeleteReceiverResponse, error) {
-	fmt.Println("delete receiver")
-
-	if req.Rid == "" {
-		return nil, status.Errorf(codes.InvalidArgument, "the id filed if empty")
-	}
+	s.logger.Debug("call DeleteReceiver method with data", zap.Any("data", req))
 
 	rid, err := uuid.Parse(req.Rid)
 	if err != nil {
-		return nil, err
+		s.logger.Error("failed to parse Rid to uuid format", zap.Error(err))
+		return nil, status.Errorf(codes.InvalidArgument, "failed to parse Rid \"%v\" to uuid format, got error: %v", req.Rid, err.Error())
 	}
 
 	err = s.DB.DeleteReceiver(ctx, rid)
 	if err != nil {
-		return nil, err
+		s.logger.Error("failed to delete receiver", zap.Error(err))
+		return nil, status.Errorf(codes.Internal, "failed to delete receiver, got error: %v", err.Error())
 	}
 
+	s.logger.Debug("receiver deleted successful")
 	return &api.DeleteReceiverResponse{}, nil
 }
